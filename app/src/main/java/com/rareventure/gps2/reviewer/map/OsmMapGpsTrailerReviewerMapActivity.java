74d287a0ed28eb44e3191f1824aa85e7d5b53510
Tim Engler <engler@gmail.com>
2019-05-06 07:53:39 +0800

    Removed unneeded comments

--- app/src/main/java/com/rareventure/gps2/reviewer/map/OsmMapGpsTrailerReviewerMapActivity.java
+++ app/src/main/java/com/rareventure/gps2/reviewer/map/OsmMapGpsTrailerReviewerMapActivity.java
@@ -627,8 +627,6 @@ GTGEventListener
 					((TextView)convertView.findViewById(R.id.totalDist)).setText(MapScaleWidget.calcLabelForLength(gpsTrailerOverlay.sas.getTotalDistM(),
 							GTG.prefs.useMetric));
 					((TextView)convertView.findViewById(R.id.timesInArea)).setText(String.valueOf(gpsTrailerOverlay.sas.getTimesInArea()));
-					//((TextView)convertView.findViewById(R.id.timesInArea)).setText(String.valueOf(gpsTrailerOverlay.sas.getTimesInArea()));
-					// line executed twice
 					
 					TimeZone tz = gpsTrailerOverlay.sas.timeZone;
 					
