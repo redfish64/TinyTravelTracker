0a6c4e788c19c98cca459af63de5d842dfa85e80
Igor Slobodskov <igor.slobodskov@phystech.edu>
2019-07-11 15:02:46 +0300

    rm duplicated code, add "final" to consts.

--- app/src/main/java/com/rareventure/gps2/GpsTrailerGpsStrategy.java
+++ app/src/main/java/com/rareventure/gps2/GpsTrailerGpsStrategy.java
@@ -23,7 +23,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 import android.os.SystemClock;
-import android.util.Log;
 
 import com.rareventure.android.GpsReader;
 import com.rareventure.android.IntentTimer;
@@ -237,7 +236,7 @@ public class GpsTrailerGpsStrategy {
 		 */
 		protected boolean updateFromStrategyThreadOnly(long deltaTimeMs) {
 			intentTimer.writeDebug("deltaTimeMs "+deltaTimeMs);
-			long timeFromPhoneBootMs = SystemClock.elapsedRealtime();
+			final long timeFromPhoneBootMs = SystemClock.elapsedRealtime();
 
 			//first lets update the stats so far
 			if(gpsOn)
@@ -264,7 +263,7 @@ public class GpsTrailerGpsStrategy {
 			
 			if(gpsOn)
 			{
-				long currentTimeGpsRunning = timeFromPhoneBootMs - gpsAttemptStartedFromPhoneBootMs;
+				final long currentTimeGpsRunning = timeFromPhoneBootMs - gpsAttemptStartedFromPhoneBootMs;
 
 				//if the desiremanager is satisfied, we successfully read a gps point, or
 				//the absolute time left to perform a gps reading is used up,
@@ -280,7 +279,7 @@ public class GpsTrailerGpsStrategy {
 					}
 					else
 						desireManager.updateDesiresForUnsuccessfulReading(gpsTimeAvailable);
-					
+
 					nextSignificantEvent = desireManager.waitTimeMs + timeFromPhoneBootMs;
 					
 					return false;
@@ -661,8 +660,8 @@ public class GpsTrailerGpsStrategy {
 
 			//reset long time wanted back to the minimum whenever we get a reading
 			longTimeWanted = (long) (prefs.minLongTimeOfShortTimeMultiplier * shortTimeWanted) + 1;
-			
-			waitTimeMs = calculateAbsTimeNeeded((long) (shortTimeWanted * prefs.extraWaitTimeShortTimeMultiplier + shortTimeWanted)+1);
+
+			waitTimeMs = calculateAbsTimeNeeded(getTotalWantedGpsTimeMs());
 			currTimeWanted = shortTimeWanted;
 		}
 	
@@ -680,7 +679,7 @@ public class GpsTrailerGpsStrategy {
 			//we need to budget our time for gps so we can run short runs periodically, and
 			//if enough time has passed, then do a long run
 
-			long totalWantedGpsTimeMs = (long) (shortTimeWanted * prefs.extraWaitTimeShortTimeMultiplier + shortTimeWanted)+1;
+			final long totalWantedGpsTimeMs = getTotalWantedGpsTimeMs();
 			waitTimeMs = calculateAbsTimeNeeded(totalWantedGpsTimeMs);
 			
 			//if there is enough time to do a long run
@@ -704,6 +703,10 @@ public class GpsTrailerGpsStrategy {
 				
 			}
 			
+		}
+
+		private long getTotalWantedGpsTimeMs() {
+			return (long) (shortTimeWanted * prefs.extraWaitTimeShortTimeMultiplier + shortTimeWanted)+1;
 		}
 
 		/**
